ac454d5a04d03503e858fdc2ed0aeec142b3c954
Xavier Collantes <xcollantes@zagmail.gonzaga.edu>
2017-09-16 00:11:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function happyHolidayTo(holiday, name)
 {
   return "Happy ${holiday}, ${name}!";
 }
-fucntion holidayCountdown(days, holiday)
+function holidayCountdown(days, holiday)
 {
-  return "It's ${days} days until ${holiday}!""
+  return "It's ${days} days until ${holiday}!";
 }
